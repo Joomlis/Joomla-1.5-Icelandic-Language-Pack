9e7e140f72516a81b16e5ec38f24294158ebfdb6
Gunnar Guðvarðarson <gunni@wave.is>
2011-01-06 03:44:50 +0000

    initial import

---
+++ language/is-IS/is-IS.ignore.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @version		$Id: is-IS.ignore.php 10381 2008-07-15  - Þýtt: MagnúsG $
+* @package		Joomlís! samfélagið - http://www.joomlis.net
+* @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+$search_ignore[] = "og";
+$search_ignore[] = "í";
+$search_ignore[] = "á";
+?>
